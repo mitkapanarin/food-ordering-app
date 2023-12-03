81f9cd6937c3a472dd07503b1f7252d9ae000289
Mitka Panarin <114948515+mitkapanarin@users.noreply.github.com>
2023-12-03 15:16:06 +0100

    pages added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navigation from './navigation/Navigation'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className="bg-black text-white">
+      Cart
+    </div>
+  )
+}
+
+export default Cart
