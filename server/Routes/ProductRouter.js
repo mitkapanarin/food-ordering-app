1f4d37f37c24a0e12b8033c91a475d97c91bd7a9
mitkapanarin <mitka_j@yahoo.com>
2023-12-10 00:09:00 +0100

    server started

---
+++ server/Routes/ProductRouter.js
@@ -0,0 +1,28 @@
+import express from "express"
+
+export const Product = express.Router()
+
+Product.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.status(200).send({ data: products})
+    } catch (err) {
+        res.status(400).send({ error: err})
+    }
+})
+
+Product.get('/products-by-categories', async(req, res) => {
+    try {
+        const products = await Product.aggregate([
+            { $match: {}},
+            { $group: {
+                _id: '$category',
+                products: { $push: '$$ROOT'}
+            }},
+            { $project: { name: '$_id', products: 1, _id: 0}}
+        ])
+        res.status(200).send({ data: products})
+    } catch (err) {
+        res.status(400).send({ error: err})
+    }
+})
--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from  "mongoose"
+import { Product } from "./Routes/ProductRouter.js"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("api/product", Product)
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+const PORT = process.env.PORT || 6001
+app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`))
